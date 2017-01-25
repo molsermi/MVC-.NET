d2c53ecc2dffa7b51e789e043131f52b52bbfd1e
molsermi <sergiu.moldovan1994@gmail.com>
2017-01-25 21:40:13 +0200

    Teme.

---
+++ Teme cursul 8/Curs8 MVC/Controllers/EvaluationController.cs
@@ -0,0 +1,106 @@
+﻿using Curs8_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Curs8_MVC.Controllers
+{
+    public class EvaluationController : Controller
+    {
+
+        public static List<ProjectEvaluation> ListOfProject = new List<ProjectEvaluation>
+        {
+            new ProjectEvaluation() {name="Transilvania", city = "Cluj", country="Romania", rating= 9.25, id=1},
+            new ProjectEvaluation() {name="Oltenia", city = "<script> alert('xss'); </script>", country="Romania", rating= 4.65, id=11 },
+            new ProjectEvaluation() {name="Ardeal", city = "Alba", country="Romania", rating= 6.8,id=111 }
+        };
+
+        // GET: Evaluation
+        public ActionResult Index()
+        {
+            return View(ListOfProject);
+        }
+
+        // GET: Evaluation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Evaluation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Evaluation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var evaluation = ListOfProject.Single(e => e.id == id);
+
+            if (TryUpdateModel(evaluation))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(evaluation);
+
+            //try
+            //{
+            //    // TODO: Add update logic here
+
+            //    return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+        }
+
+        // GET: Evaluation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Teme cursul 8/Curs8 MVC/Models/ProjectEvaluation.cs
@@ -0,0 +1,45 @@
+﻿using Curs8_MVC.CustomAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Curs8_MVC.Models
+{
+    [Bind(Exclude = "Country")]
+    //[Bind(Include = "all the other properties")]
+
+    public class ProjectEvaluation
+    {
+
+        [MaxWordsAttribute(3)]
+        public string name { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        [MinWordsAttribute(2)]
+        public string city { get; set; }
+        
+        public string country { get; set; }
+
+        [Range(1, 10)]
+        [Required]
+        public double rating { get; set; }
+
+        public int id { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (rating < 2 && name.ToLower().StartsWith("jack"))
+                yield return new ValidationResult("Sorry !!!");
+        }
+
+        public IEnumerable<ValidationResult> Validare(ValidationContext validationContext)
+        {
+            if (rating > 8 && name.ToLower().StartsWith("sergiu"))
+                yield return new ValidationResult("A very good rating!");
+        }
+    }
+}
